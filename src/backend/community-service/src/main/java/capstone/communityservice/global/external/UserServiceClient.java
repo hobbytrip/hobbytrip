f81850c9d68d67c9e0d0e584ace11340c04bf9c5
HanYoonSoo <hdbstn3055@gmail.com>
2024-04-02 19:34:56 +0900

    refactor: UserServiceClient

--- src/backend/community-service/src/main/java/capstone/communityservice/global/external/UserServiceClient.java
+++ src/backend/community-service/src/main/java/capstone/communityservice/global/external/UserServiceClient.java
@@ -5,8 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-//@FeignClient(name = "user-service")
-//public interface UserServiceClient {
-//    @GetMapping("/{email}")
-//    UserFeignResponseDto getUser(@PathVariable("email") String email);
-//}
+@FeignClient(name = "user-service")
+public interface UserServiceClient {
+    @GetMapping("/{email}")
+    UserFeignResponseDto getUser(@PathVariable("email") String email);
+}
