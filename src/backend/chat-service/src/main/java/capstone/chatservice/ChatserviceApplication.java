a1eb6d78ef9edde056e995493ad3063aed28a9c3
acs0209 <annjae4676@naver.com>
2024-04-03 19:47:45 +0900

    feat: init project

---
+++ src/backend/chat-service/src/main/java/capstone/chatservice/ChatserviceApplication.java
@@ -0,0 +1,13 @@
+package capstone.chatservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatserviceApplication.class, args);
+	}
+
+}
---
+++ src/backend/chat-service/src/test/java/capstone/chatservice/ChatserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone.chatservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
