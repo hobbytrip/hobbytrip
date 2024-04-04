8deb16ab1dea062c20623680b79c434d2da7ca6f
ryudain05 <dkdlsekdls@naver.com>
2024-04-04 23:58:50 +0900

    feat: Jwt 필터 적용

--- src/backend/user-service/src/main/java/com/capstone/userservice/global/util/TokenUtil.java
+++ src/backend/user-service/src/main/java/com/capstone/userservice/global/util/TokenUtil.java
@@ -3,21 +3,35 @@ package com.capstone.userservice.global.util;
 
 import com.capstone.userservice.domain.user.dto.CustomUserInfoDto;
 import com.capstone.userservice.global.dto.JwtTokenDto;
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
-
-import java.security.Key;
-import java.util.Date;
 
 @Slf4j
 @Component
 public class TokenUtil {
     private static final String USER_ID = "userId";
     private static final String BEARER_TYPE = "Bearer";
+    private static final String AUTHORITIES_KEY = "auth";
     private final Key key;
     private final long accessTokenExpTime;
     private final long refreshTokenExpTime;
@@ -31,7 +45,13 @@ public class TokenUtil {
         this.refreshTokenExpTime = refreshTokenExpTime;
     }
 
-    public JwtTokenDto generateToken(CustomUserInfoDto user) {
+    public JwtTokenDto generateToken(CustomUserInfoDto user, Authentication authentication) {
+
+        //권한 'ROLE_USER'
+        String authorities = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
         long now = (new Date()).getTime();
         Date accessTokenExpiresIn = new Date(now + accessTokenExpTime);
 
@@ -45,6 +65,7 @@ public class TokenUtil {
          */
         String accessToken = Jwts.builder()
                 .claim(USER_ID, user.getUserId())
+                .claim(AUTHORITIES_KEY, authorities)
                 .setSubject(user.getEmail())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(accessTokenExpiresIn)
@@ -69,13 +90,28 @@ public class TokenUtil {
     }
 
     /**
-     * Token 에서 UserEmail 추출
+     * Token 에서 유저 권한 추출 -> authentication 에 저장
      *
      * @param accessToken
-     * @return userEmail
+     * @return authentication
      */
-    public String getUserEmail(String accessToken) {
-        return parseClaims(accessToken).getSubject();
+    public Authentication getAuthentication(String accessToken) {
+        //토큰 복호화
+        Claims claims = parseClaims(accessToken);
+
+        if (claims.get(AUTHORITIES_KEY) == null) {
+            throw new RuntimeException("권한 정보가 없는 토큰입니다.");
+        }
+
+        //클레임에서 권한 정보 가져옴
+        Collection<? extends GrantedAuthority> authorities =
+                Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList());
+        //UserDetails 객체로 만들어서 Authentication 리턴
+        UserDetails principal = new User(claims.getSubject(), "", authorities);
+
+        return new UsernamePasswordAuthenticationToken(principal, "", authorities);
     }
 
     private Claims parseClaims(String accessToken) {
