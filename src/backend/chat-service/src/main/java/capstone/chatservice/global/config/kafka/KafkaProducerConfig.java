bff7fb60d266a65a9d5f35664a0ea1f9e78c287d
acs0209 <annjae4676@naver.com>
2024-04-07 01:26:18 +0900

    feat: 카프카 프로듀서, 컨슈머, admin config 구현

---
+++ src/backend/chat-service/src/main/java/capstone/chatservice/global/config/kafka/KafkaProducerConfig.java
@@ -0,0 +1,43 @@
+package capstone.chatservice.global.config.kafka;
+
+import capstone.chatservice.domain.server.dto.ServerMessageDto;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+@EnableKafka
+@Configuration
+public class KafkaProducerConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Bean
+    public Map<String, Object> producerConfig() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        config.put(org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class);
+        config.put(org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                JsonSerializer.class);
+
+        return config;
+    }
+
+    public ProducerFactory<String, ServerMessageDto> serverChatProducerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfig());
+    }
+
+    @Bean
+    public KafkaTemplate<String, ServerMessageDto> serverChatKafkaTemplate() {
+        return new KafkaTemplate<>(serverChatProducerFactory());
+    }
+}
