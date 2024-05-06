e38c0d40e9f9a3fd1538e4b820303e9437a67e2d
ryudain05 <dkdlsekdls@naver.com>
2024-05-06 23:23:56 +0900

    feat: friend entity 설정

---
+++ src/backend/user-service/src/main/java/com/capstone/userservice/domain/friend/entity/Friendship.java
@@ -0,0 +1,47 @@
+package com.capstone.userservice.domain.friend.entity;
+
+
+import com.capstone.userservice.domain.user.entity.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Friendship {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User users;
+
+    private String userEmail;
+    private String friendEmail;
+    private FriendshipStatus status;
+    private boolean isFrom;
+
+    private Long counterpartId;
+
+    public void acceptFriendshipRequest() {
+        status = FriendshipStatus.ACCEPT;
+    }
+
+    public void setCounterpartId(Long id) {
+        counterpartId = id;
+    }
+
+}
