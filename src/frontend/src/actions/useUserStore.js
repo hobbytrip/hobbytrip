0066b7e271c988832e54614fa4d5ab151dc0330b
yeahzee0421 <ydw08250421@gmail.com>
2024-05-11 21:33:05 +0900

    [refactor] 사용자 정보 조회 함수 추가 및 리팩토링

--- src/frontend/src/actions/useUserStore.js
+++ src/frontend/src/actions/useUserStore.js
@@ -4,6 +4,19 @@ import axios from "../utils/instance";
 const useUserStore = create((set) => ({
   user: null,
   setUserInfo: (userInfo) => set({ user: userInfo }),
+  getUserInfo: async () => {
+    try {
+      const response = await axios.get(`/user/profile`);
+      if (response.status === 200) {
+        const userData = response.data.data;
+        set({ user: userData });
+      } else {
+        console.log("Falied to fetch user data");
+      }
+    } catch (error) {
+      console.log("Error fetching user data:", error);
+    }
+  },
   updateUserInfo: async (endpoint, updates) => {
     try {
       const response = await axios.patch(`/user/profile/${endpoint}`, updates);
@@ -12,7 +25,7 @@ const useUserStore = create((set) => ({
           user: { ...state.user, ...updates },
         }));
       } else {
-        console.error("Failed to update user data on the server");
+        console.error("Failed to update user data");
       }
     } catch (error) {
       console.error("Error updating user data:", error);
