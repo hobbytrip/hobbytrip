277275f27850bdb0321a6b8e46d2cb445099b93b
acs0209 <annjae4676@naver.com>
2024-05-13 00:55:04 +0900

    feat: init project

---
+++ src/backend/state-service/src/main/java/capstone/stateservice/StateserviceApplication.java
@@ -0,0 +1,13 @@
+package capstone.stateservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StateserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StateserviceApplication.class, args);
+	}
+
+}
