657da3d5203f4690a526b23d4e6bb3d129eb1f8c
yeahzee0421 <ydw08250421@gmail.com>
2024-05-16 16:06:55 +0900

    [refactor] #61 - useAuthStore 토큰 관리 방식 수정

--- src/frontend/src/actions/useAuthStore.js
+++ src/frontend/src/actions/useAuthStore.js
@@ -1,36 +1,24 @@
 import create from "zustand";
-import { useCookies } from "react-cookie";
 
 const useAuthStore = create((set, get) => {
-  const [cookies, setCookie, removeCookie] = useCookies([
-    "accessToken",
-    "refreshToken",
-  ]);
+  const accesstoken = localStorage.getItem("accesstoken");
+  const refreshtoken = localStorage.getItem("refreshtoken");
 
   return {
-    accessToken: cookies.accessToken,
-    refreshToken: cookies.refreshToken,
+    accesstoken,
+    refreshtoken,
 
     login: async (email, password) => {
       try {
-        const response = await axios.post("/login", {
+        const response = await axios.post("/user/login", {
           email,
           password,
         });
-        const { accessToken, refreshToken } = response.data;
-        if (accessToken && refreshToken) {
-          //path: / : 도메인의 모든 경로에 대해 쿠키 유효
-          setCookie("accessToken", accessToken, {
-            path: "/",
-            secure: true,
-            sameSite: "strict",
-          });
-          setCookie("refreshToken", refreshToken, {
-            path: "/",
-            secure: true,
-            sameSite: "strict",
-          });
-          set({ accessToken, refreshToken });
+        const { accesstoken, refreshtoken } = response.data;
+        if (accesstoken && refreshtoken) {
+          localStorage.setItem("accesstoken", accesstoken);
+          localStorage.setItem("refreshtoken", refreshtoken);
+          set({ accesstoken, refreshtoken });
           console.log("로그인 성공");
         }
       } catch (error) {
@@ -41,15 +29,14 @@ const useAuthStore = create((set, get) => {
 
     logout: async () => {
       try {
-        const { accessToken, refreshToken } = get();
-        const response = await axios.post("/logout", {
-          accessToken,
-          refreshToken,
+        const response = await axios.post("/user/logout", {
+          accesstoken,
+          refreshtoken,
         });
-        if (response.data) {
-          removeCookie("accessToken", { path: "/" });
-          removeCookie("refreshToken", { path: "/" });
-          set({ accessToken: undefined, refreshToken: undefined });
+        if (response.status == 200) {
+          localStorage.removeItem("accesstoken");
+          localStorage.removeItem("refreshtoken");
+          set({ accesstoken: undefined, refreshtoken: undefined });
           console.log("로그아웃 완료");
         }
       } catch (error) {
