b9b45c874b4e5bc74e8b6e2bdf648bf1fdf58c21
acs0209 <annjae4676@naver.com>
2024-05-16 19:37:52 +0900

    feat: state service OpenFeign 구현

---
+++ src/backend/chat-service/src/main/java/capstone/chatservice/infra/client/StateServiceClient.java
@@ -0,0 +1,14 @@
+package capstone.chatservice.infra.client;
+
+import capstone.chatservice.global.common.dto.DataResponseDto;
+import capstone.chatservice.infra.kafka.dto.ConnectionStateInfo;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "state-service")
+public interface StateServiceClient {
+
+    @PostMapping("/connection/state")
+    DataResponseDto<String> saveConnectionStateInfo(@RequestBody ConnectionStateInfo sessionDto);
+}
