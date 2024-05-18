a83fabd7a2dc1133cdca457073d6ace64ebbc869
minhong1126 <mjhong159357@gmail.com>
2024-05-19 00:23:10 +0900

    [feat] #26 - api 수정 및 주석 추가

--- src/frontend/src/components/MediaView/UserVideoComponent.jsx
+++ src/frontend/src/components/MediaView/UserVideoComponent.jsx
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
-import VideoComponent from './VideoComponent/VideoComponent';
+import VideoComponent from './VideoComponent';
 
 class UserVideoComponent extends Component {
   render() {
     return (
       <div>
         {this.props.streamManager !== undefined ? (
-          <div className="streamcomponent">
             <VideoComponent streamManager={this.props.streamManager} />
-            {/* <div><p>{this.getNicknameTag()}</p></div> */}
-          </div>
         ) : null}
       </div>
     );
