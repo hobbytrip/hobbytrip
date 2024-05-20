275a7aea2205311a16a4a99c32037ef693021742
yeahzee0421 <ydw08250421@gmail.com>
2024-05-20 11:12:12 +0900

    [style] 내 행성 아이콘 style 수정

--- src/frontend/src/components/MainView/MyPlanet/MyPlanet.jsx
+++ src/frontend/src/components/MainView/MyPlanet/MyPlanet.jsx
@@ -1,46 +1,65 @@
-import style from './MyPlanet.module.css';
+import { useEffect } from "react";
+import style from "./MyPlanet.module.css";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import { FaPlus } from "react-icons/fa6";
-
+import usePlanetIcon from "../../../hooks/usePlanetIcon";
 
 const Leftbtn = () => {
-    return (
-        <button className={style.leftBtn}>
-            <IoIosArrowBack style={{ width: '15px', height: '15px', color: '#D9D9D9'}} />
-        </button>
-    );
-}
+  return (
+    <button className={style.leftBtn}>
+      <IoIosArrowBack
+        style={{ width: "15px", height: "15px", color: "#D9D9D9" }}
+      />
+    </button>
+  );
+};
 const Rightbtn = () => {
-    return (
-        <button className={style.rightBtn}>
-            <IoIosArrowForward style={{ width: '15px', height: '15px', color: '#D9D9D9' }} />
-        </button>
-    );
-}
+  return (
+    <button className={style.rightBtn}>
+      <IoIosArrowForward
+        style={{ width: "15px", height: "15px", color: "#D9D9D9" }}
+      />
+    </button>
+  );
+};
 const CreatePlanetbtn = () => {
-    return (
-        <button className={style.createPlanet}>
-            <FaPlus style={{ width: '16px', height: '16px', color: '#EDEDED' }} />
-        </button>
-    );
-}
+  return (
+    <button className={style.createPlanet}>
+      <FaPlus
+        className={style.plusIcon}
+        style={{ width: "16px", height: "16px", color: "#EDEDED" }}
+      />
+    </button>
+  );
+};
 
 const MyPlanet = () => {
-    return (
-        <div className={style.wrapper}>
-            <h3> 내 행성 </h3>
-            <div className={style.planetContainer}>  
-                <Leftbtn />
-                <div className={style.planetList}>
-                    <div>  
-                        <button> B1 </button>
-                        <CreatePlanetbtn />
-                    </div>
-                </div>
-                <Rightbtn />  
-            </div>
+  const [planetIcon, getRandomPlanetIcon] = usePlanetIcon();
+  useEffect(() => {
+    getRandomPlanetIcon();
+  }, [getRandomPlanetIcon]);
+  return (
+    <div className={style.wrapper}>
+      <h3> 내 행성 </h3>
+      <div className={style.planetContainer}>
+        <Leftbtn />
+        <div className={style.planetList}>
+          <div>
+            <button className={style.planetThumb}>
+              {" "}
+              <img
+                src={planetIcon}
+                className={style.planetIcon}
+                alt="행성이미지"
+              />
+            </button>
+            <CreatePlanetbtn />
+          </div>
         </div>
-    );
-}
+        <Rightbtn />
+      </div>
+    </div>
+  );
+};
 
 export default MyPlanet;
