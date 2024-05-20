c09184ba8850dd2b6f50f695bb4e6214c9868c8e
ryudain05 <dkdlsekdls@naver.com>
2024-05-20 19:26:53 +0900

    refactor: 설정 수정

--- src/backend/user-service/src/main/java/com/capstone/userservice/domain/friend/service/FriendsStatusClient.java
+++ src/backend/user-service/src/main/java/com/capstone/userservice/domain/friend/service/FriendsStatusClient.java
@@ -5,8 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "${api.authentication-server.name}",
-        url = "${api.authentication-server.url}")
+@FeignClient(name = "${state-service}")
 public interface FriendsStatusClient {
 
     @GetMapping("/friends/{userId}")
