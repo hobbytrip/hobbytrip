9295dfb30bc2962d462a547b7ef354ec5e6c3fc8
yeahzee0421 <ydw08250421@gmail.com>
2024-05-20 14:29:59 +0900

    [feat] #37 - 서버 생성 컴포넌트 연결

--- src/frontend/src/components/MainView/MyPlanet/MyPlanet.jsx
+++ src/frontend/src/components/MainView/MyPlanet/MyPlanet.jsx
@@ -1,8 +1,10 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import style from "./MyPlanet.module.css";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import { FaPlus } from "react-icons/fa6";
+import { AiOutlineClose } from "react-icons/ai";
 import usePlanetIcon from "../../../hooks/usePlanetIcon";
+import CreateServer from "../../Modal/ServerModal/CreateServer/CreateServer";
 
 const Leftbtn = () => {
   return (
@@ -22,9 +24,9 @@ const Rightbtn = () => {
     </button>
   );
 };
-const CreatePlanetbtn = () => {
+const CreatePlanetbtn = ({ onClick }) => {
   return (
-    <button className={style.createPlanet}>
+    <button className={style.createPlanet} onClick={onClick}>
       <FaPlus
         className={style.plusIcon}
         style={{ width: "16px", height: "16px", color: "#EDEDED" }}
@@ -35,9 +37,18 @@ const CreatePlanetbtn = () => {
 
 const MyPlanet = () => {
   const [planetIcon, getRandomPlanetIcon] = usePlanetIcon();
+  const [showCreateServer, setShowCreateServer] = useState(false);
+
   useEffect(() => {
     getRandomPlanetIcon();
   }, [getRandomPlanetIcon]);
+
+  const handleCreateModalClick = () => {
+    setShowCreateServer(true);
+  };
+  const handleCloseModal = () => {
+    setShowCreateServer(false);
+  };
   return (
     <div className={style.wrapper}>
       <h3> 내 행성 </h3>
@@ -46,18 +57,30 @@ const MyPlanet = () => {
         <div className={style.planetList}>
           <div>
             <button className={style.planetThumb}>
-              {" "}
               <img
                 src={planetIcon}
                 className={style.planetIcon}
                 alt="행성이미지"
               />
             </button>
-            <CreatePlanetbtn />
+            <CreatePlanetbtn onClick={handleCreateModalClick} />
           </div>
         </div>
         <Rightbtn />
       </div>
+      {showCreateServer && (
+        <div className={style.createServerModal}>
+          <CreateServer />
+          <button className={style.closeBtn} onClick={handleCloseModal}>
+            {/* <AiOutlineClose
+              style={{ width: "20px", height: "20px", color: "#FFFFFF" }}
+            ,/> */}
+            <h4 style={{ color: "#fff", textDecoration: "underline" }}>
+              뒤로 가기
+            </h4>
+          </button>
+        </div>
+      )}
     </div>
   );
 };
