f6527f6a608f0b91dc085d46468923e3fb2d2e6b
acs0209 <annjae4676@naver.com>
2024-05-26 00:32:48 +0900

    feat: 유저 채널 위치 상태 관리 로직 구현

--- src/backend/state-service/src/main/java/capstone/stateservice/infra/kafka/consumer/location/UserLocationEventConsumer.java
+++ src/backend/state-service/src/main/java/capstone/stateservice/infra/kafka/consumer/location/UserLocationEventConsumer.java
@@ -1,5 +1,6 @@
 package capstone.stateservice.infra.kafka.consumer.location;
 
+import capstone.stateservice.domain.location.service.command.LocationStateCommandService;
 import capstone.stateservice.infra.kafka.consumer.location.dto.UserLocationEventDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,9 +12,10 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class UserLocationEventConsumer {
 
+    private final LocationStateCommandService locationStateCommandService;
 
     @KafkaListener(topics = "${spring.kafka.topic.user-location-event}", groupId = "${spring.kafka.consumer.group-id.user-location-event}", containerFactory = "userLocationEventListenerContainerFactory")
     public void userLocationEventListener(UserLocationEventDto userLocationEventDto) {
-
+        locationStateCommandService.saveLocationState(userLocationEventDto);
     }
 }
