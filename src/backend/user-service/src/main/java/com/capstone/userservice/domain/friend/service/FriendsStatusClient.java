dbb30506d9e3ef67fbd87c8621b2f5b655708f3f
ryudain05 <dkdlsekdls@naver.com>
2024-05-26 23:45:13 +0900

    refactor: FeignClinet name 수정

--- src/backend/user-service/src/main/java/com/capstone/userservice/domain/friend/service/FriendsStatusClient.java
+++ src/backend/user-service/src/main/java/com/capstone/userservice/domain/friend/service/FriendsStatusClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "user-service")
+@FeignClient(name = "state-service")
 public interface FriendsStatusClient {
 
     @GetMapping("/friends/{userId}")
