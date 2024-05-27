c4c7d3d1c968dd3e9a199a41584d639c8c1d80bd
acs0209 <annjae4676@naver.com>
2024-05-27 14:32:43 +0900

    refactor: openfeign 경로 수정

--- src/backend/chat-service/src/main/java/capstone/chatservice/infra/client/CommunityServiceClient.java
+++ src/backend/chat-service/src/main/java/capstone/chatservice/infra/client/CommunityServiceClient.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "community-service")
 public interface CommunityServiceClient {
 
-    @GetMapping("/community/check/room/guild/{userId}")
+    @GetMapping("/feign/{userId}")
     UserServerDmInfo getServerIdsAndRoomIds(@PathVariable(value = "userId") Long userId);
 }
