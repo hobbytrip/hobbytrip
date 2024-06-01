b6547a650714031ce4329f57d945a7216a871bb3
acs0209 <annjae4676@naver.com>
2024-06-01 13:43:04 +0900

    refactor: @SpringBootTest 주석 처리

--- src/backend/state-service/src/test/java/capstone/stateservice/StateserviceApplicationTests.java
+++ src/backend/state-service/src/test/java/capstone/stateservice/StateserviceApplicationTests.java
@@ -1,13 +1,12 @@
 package capstone.stateservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class StateserviceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
