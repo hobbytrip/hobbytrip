c31be77de1e94e03bd61f7a2aa780f47369730e0
yeahzee0421 <ydw08250421@gmail.com>
2024-06-02 01:53:37 +0900

    수정

--- src/frontend/src/components/Modal/ChatModal/CreateChatModal/ChatModal.jsx
+++ src/frontend/src/components/Modal/ChatModal/CreateChatModal/ChatModal.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { json, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import MessageSender from "../CreateChatModal/MessageSender/MessageSender";
 import useWebSocketStore from "../../../../actions/useWebSocketStore";
 import API from "../../../../utils/API/API";
@@ -9,9 +9,7 @@ export default function ChatModal({ userId, writer, onNewMessage, client }) {
   const { serverId, channelId } = useParams();
   const [chatList, setChatList] = useState([]);
   const [typingUsers, setTypingUsers] = useState([]);
-  const { sendMessage } = useWebSocketStore((state) => ({
-    sendMessage: state.sendMessage,
-  }));
+  const [file, setFile] = useState(null);
 
   useEffect(() => {
     if (client) {
@@ -19,37 +17,40 @@ export default function ChatModal({ userId, writer, onNewMessage, client }) {
         client.subscribe(API.SUBSCRIBE_CHAT(serverId), (frame) => {
           try {
             const parsedMessage = JSON.parse(frame.body);
+
             if (
               parsedMessage.actionType === "TYPING" &&
-              parsedMessage.writer !== userId
+              parsedMessage.userId !== userId
             ) {
-              setTypingUsers((prev) => [...prev, parsedMessage.writer]);
-              setTimeout(() => {
-                setTypingUsers((prev) =>
-                  prev.filter((user) => user !== parsedMessage.writer)
-                );
-              }, 2000);
-            } else if (parsedMessage.actionType === "SEND") {
-              setChatList((prevMsgs) => [...prevMsgs, parsedMessage]);
+              setTypingUsers((prevTypingUsers) => {
+                if (!prevTypingUsers.includes(parsedMessage.writer)) {
+                  return [...prevTypingUsers, parsedMessage.writer];
+                }
+                return prevTypingUsers;
+              });
+            } else if (parsedMessage.actionType === "STOP_TYPING") {
+              setTypingUsers((prevTypingUsers) =>
+                prevTypingUsers.filter(
+                  (username) => username !== parsedMessage.writer
+                )
+              );
+            } else if (
+              parsedMessage.actionType === "SEND" &&
+              parsedMessage.userId !== userId
+            ) {
+              setChatList((prevChatList) => [...prevChatList, parsedMessage]);
               if (onNewMessage) {
                 onNewMessage(parsedMessage);
               }
-            } else if (parsedMessage.actionType === "MODIFY") {
-              setChatList((prevMsgs) =>
-                prevMsgs.map((msg) =>
-                  msg.messageId === parsedMessage.messageId
-                    ? { ...msg, content: parsedMessage.content }
-                    : msg
-                )
-              );
-            } else if (parsedMessage.actionType === "DELETE") {
-              setChatList((prevMsgs) =>
-                prevMsgs.map((msg) =>
-                  msg.messageId === parsedMessage.messageId
-                    ? { ...msg, deleted: true }
-                    : msg
-                )
-              );
+            } else if (
+              parsedMessage.actionType === "SEND" &&
+              parsedMessage.files
+            ) {
+              setChatList((prevChatList) => [...prevChatList, parsedMessage]);
+              if (onNewMessage) {
+                onNewMessage(parsedMessage);
+              }
+              setFile(parsedMessage.files[0]);
             }
           } catch (error) {
             console.error("구독 오류", error);
@@ -68,28 +69,27 @@ export default function ChatModal({ userId, writer, onNewMessage, client }) {
       profileImage: "ho",
       writer,
       content: messageContent,
-      createdAt: new Date().toISOString(), // 현재시간
+      createdAt: new Date().toISOString(),
     };
-
-    let newMessage = messageBody;
-
-    if (uploadedFile !== null) {
+    try {
       const formData = new FormData();
       const jsonMsg = JSON.stringify(messageBody);
       const req = new Blob([jsonMsg], { type: "application/json" });
       formData.append("createRequest", req);
-      formData.append("files", uploadedFile);
-      const response = await axios.post(API.FILE_UPLOAD, formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      });
-    }
-
-    sendMessage(API.SEND_CHAT, newMessage);
-    setChatList((prevChatList) => [...prevChatList, newMessage]);
-    if (onNewMessage) {
-      onNewMessage(newMessage);
+      if (uploadedFile) {
+        formData.append("files", uploadedFile);
+        await axios.post(API.FILE_UPLOAD, formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        });
+      }
+      setChatList((prevChatList) => [...prevChatList, messageBody]);
+      if (onNewMessage) {
+        onNewMessage(messageBody);
+      }
+    } catch (error) {
+      console.error("메시지 전송 오류:", error);
     }
   };
 
@@ -102,10 +102,17 @@ export default function ChatModal({ userId, writer, onNewMessage, client }) {
             : `${typingUsers.join(", ")} 입력 중입니다...`}
         </div>
       )}
+      {file && (
+        <div>
+          <p>Received file: {file.fileUrl}</p>
+          <button onClick={() => window.open(file.fileUrl, "_blank")}>
+            Download File
+          </button>
+        </div>
+      )}
 
       <MessageSender
         onMessageSend={handleSendMessage}
-        // onFileMessageSend={handleFileMessageSend}
         serverId={serverId}
         channelId={channelId}
         writer={writer}
