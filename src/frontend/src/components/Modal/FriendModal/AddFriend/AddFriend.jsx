72a80895f5e392fcd084d20e44e411a2ea2104dc
yeahzee0421 <ydw08250421@gmail.com>
2024-06-14 16:53:51 +0900

    [feat] #115 - 이메일 유효성 검증

--- src/frontend/src/components/Modal/FriendModal/AddFriend/AddFriend.jsx
+++ src/frontend/src/components/Modal/FriendModal/AddFriend/AddFriend.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import s from "./AddFriend.module.css";
 
 const AddFriend = ({ onBackClick }) => {
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const isValidEmail = validateEmail(email);
+    if (!isValidEmail) {
+      setError("올바른 이메일을 입력해주세요.");
+      return;
+    }
+    setError("");
+    console.log("친구 추가:", email);
+  };
+
+  const handleChange = (e) => {
+    setEmail(e.target.value);
+    setError("");
+  };
+
+  const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
   return (
     <div className={s.wrapper}>
       <div className={s.des}>
@@ -10,20 +34,30 @@ const AddFriend = ({ onBackClick }) => {
       </div>
 
       <div className={s.container}>
-        <form className={s.form}>
-          <input
-            type="email"
-            placeholder="친구의 이메일을 입력해요!"
-            className={s.input}
-          />
-          <button type="submit" className={s.addFriendBtn}>
-            친구 추가
-          </button>
+        <form className={s.form} onSubmit={handleSubmit} noValidate>
+          <div className={s.inputContainer}>
+            <input
+              type="email"
+              placeholder="친구의 이메일을 입력해요!"
+              className={s.input}
+              value={email}
+              onChange={handleChange}
+            />
+            <button type="submit" className={s.addFriendBtn}>
+              친구 추가
+            </button>
+          </div>
         </form>
-        <button onClick={onBackClick} className={s.backBtn}>
-          돌아가기
-        </button>
       </div>
+
+      {error && (
+        <p className={s.error}>
+          <h4>{error}</h4>
+        </p>
+      )}
+      <button onClick={onBackClick} className={s.backBtn}>
+        돌아가기
+      </button>
     </div>
   );
 };
