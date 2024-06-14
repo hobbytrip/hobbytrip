b17fd26e8c5b44f10e8380a9997cdc9022fc2f05
yeahzee0421 <ydw08250421@gmail.com>
2024-06-14 20:37:35 +0900

    [refactor] #115 - 친구 컴포넌트화

--- src/frontend/src/components/Modal/FriendModal/FriendComponent/FriendComponent.jsx
+++ src/frontend/src/components/Modal/FriendModal/FriendComponent/FriendComponent.jsx
@@ -1,31 +1,34 @@
-// import s from "./FriendComponent.module.css";
-// import { AiFillMessage } from "react-icons/ai";
-// import { VscKebabVertical } from "react-icons/vsc";
+import s from "./FriendComponent.module.css";
+import { AiFillMessage } from "react-icons/ai";
+import { VscKebabVertical } from "react-icons/vsc";
 
-// function FriendComponent() {
-//   return (
-//     <div className={s.wrapper}>
-//       <li className={s.friendContainer}>
-//         <div className={s.friendImg}>
-//           <img
-//             src="./../../../../src/assets/image/default-logo.png"
-//             alt="친구 이미지"
-//           />
-//         </div>
-//         <div className={s.friendData}>
-//           <h4 className={s.friendName}>친구 </h4>
-//           <h5 className={s.friendIntro}>친구 소개</h5>
-//         </div>
-//         <div className={s.friendFunction}>
-//           <button>
-//             <AiFillMessage style={{ width: "15.62px", height: "15.6px" }} />
-//           </button>
-//           <button>
-//             <VscKebabVertical style={{ height: "15px" }} />
-//           </button>
-//         </div>
-//       </li>
-//     </div>
-//   );
-// }
-// export default FriendComponent;
+function FriendComponent({ friend }) {
+  return (
+    <div className={s.wrapper}>
+      <li key={friend.friendId} className={s.friendContainer}>
+        <div className={s.friendImg}>
+          <img
+            src={
+              friend.friendImageUrl ||
+              "./../../../../src/assets/image/default-logo.png"
+            }
+            alt="친구 이미지"
+          />
+        </div>
+        <div className={s.friendData}>
+          <h4 className={s.friendName}>{friend.friendName}</h4>
+          <h4 className={s.friendName}>{friend.connectionState}</h4>
+        </div>
+        <div className={s.friendFunction}>
+          <button>
+            <AiFillMessage style={{ width: "15.62px", height: "15.6px" }} />
+          </button>
+          <button>
+            <VscKebabVertical style={{ height: "15px" }} />
+          </button>
+        </div>
+      </li>
+    </div>
+  );
+}
+export default FriendComponent;
--- src/frontend/src/components/Modal/FriendModal/FriendList/FriendList.jsx
+++ src/frontend/src/components/Modal/FriendModal/FriendList/FriendList.jsx
@@ -1,12 +1,10 @@
 import { useEffect, useState } from "react";
 import { axiosInstance } from "../../../../utils/axiosInstance";
-import { useQuery } from "@tanstack/react-query";
 import API from "../../../../utils/API/API";
 import useUserStore from "../../../../actions/useUserStore";
 import useFriendStore from "../../../../actions/useFriendStore";
 import s from "./FriendList.module.css";
-import { AiFillMessage } from "react-icons/ai";
-import { VscKebabVertical } from "react-icons/vsc";
+import FriendComponent from "../FriendComponent/FriendComponent";
 
 function FriendList() {
   const [dmNotice, setDmNotice] = useState([]);
@@ -32,33 +30,14 @@ function FriendList() {
 
   return (
     <div className={s.wrapper}>
-      {friends &&
+      {!friends ? (
+        <div>친구 없음.</div>
+      ) : (
+        friends &&
         friends.map((friend) => (
-          <li key={friend.id} className={s.friendContainer}>
-            <div className={s.friendImg}>
-              <img
-                src={
-                  friend.friendImageUrl ||
-                  "./../../../../src/assets/image/default-logo.png"
-                }
-                alt="친구 이미지"
-              />
-            </div>
-            <div className={s.friendData}>
-              <h4 className={s.friendName}>{friend.friendName}</h4>
-              <h4 className={s.friendEmail}>{friend.name}</h4>
-              <h4 className>{friend.connectionState}</h4>
-            </div>
-            <div className={s.friendFunction}>
-              <button>
-                <AiFillMessage style={{ width: "15.62px", height: "15.6px" }} />
-              </button>
-              <button>
-                <VscKebabVertical style={{ height: "15px" }} />
-              </button>
-            </div>
-          </li>
-        ))}
+          <FriendComponent key={friend.friendId} friend={friend} />
+        ))
+      )}
     </div>
   );
 }
