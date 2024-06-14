426e4643bb04ff3497b1a4f031acc2bf4f983a7d
ryudain05 <dkdlsekdls@naver.com>
2024-06-14 23:29:54 +0900

    fix: FeignConfig 수정

--- src/backend/user-service/src/main/java/com/capstone/userservice/global/config/FeignConfig.java
+++ src/backend/user-service/src/main/java/com/capstone/userservice/global/config/FeignConfig.java
@@ -1,10 +1,12 @@
 package com.capstone.userservice.global.config;
 
 import feign.Client;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableFeignClients("com.capstone.userservice.domain.friend.service")
 public class FeignConfig {
 
     // Ribbon 로드 밸런서를 사용하는 것이 기본값이라 이 부분을 null로 처리
