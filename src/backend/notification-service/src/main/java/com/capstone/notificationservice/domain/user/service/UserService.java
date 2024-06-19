d5f10c2485b2ffbaad5cdc35068a31b17c41ba23
youcalste03 <luckthb325@naver.com>
2024-06-20 01:37:36 +0900

    fix: userRepository log추가

--- src/backend/notification-service/src/main/java/com/capstone/notificationservice/domain/user/service/UserService.java
+++ src/backend/notification-service/src/main/java/com/capstone/notificationservice/domain/user/service/UserService.java
@@ -4,16 +4,19 @@ import com.capstone.notificationservice.domain.user.entity.User;
 import com.capstone.notificationservice.domain.user.repository.UserRepository;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserService {
     private final UserRepository userRepository;
 
     @Transactional
     public Optional<User> findUser(Long userId) {
+        log.info("userId0 :{}",userRepository.findById(userId));
         return userRepository.findById(userId);
     }
 
