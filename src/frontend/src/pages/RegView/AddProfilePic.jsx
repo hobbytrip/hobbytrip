45eea7a6bc18b5a3685f18d9f13a812ac2f88ab8
yeahzee0421 <ydw08250421@gmail.com>
2024-06-19 21:39:51 +0900

    [init] #159 - 유저 프로필 추가 init

---
+++ src/frontend/src/pages/RegView/AddProfilePic.jsx
@@ -0,0 +1,13 @@
+import s from "./RegView.module.css";
+
+function AddProfilePic() {
+  return (
+    <div className={s.wrapper}>
+      <div className={s.container}>
+        <div className={s.contents}></div>
+      </div>
+    </div>
+  );
+}
+
+export default AddProfilePic;
